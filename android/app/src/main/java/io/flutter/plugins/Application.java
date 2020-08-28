067b7ea3023a0494b31c83aba928cada78d5da91
fayaz <fayazfz07@gmail.com>
2020-08-28 23:09:43 +0530

    - fixed FCM in Native code

--- android/app/src/main/java/io/flutter/plugins/Application.java
+++ android/app/src/main/java/io/flutter/plugins/Application.java
@@ -5,13 +5,13 @@ import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-//import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
 
 public class Application extends FlutterApplication implements PluginRegistrantCallback {
     @Override
     public void onCreate() {
         super.onCreate();
-//        FlutterFirebaseMessagingService.setPluginRegistrant(this);
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
     }
 
     @Override
